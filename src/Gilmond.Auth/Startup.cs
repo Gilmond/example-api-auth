83d8d670443760440d80f936266cba21ab4251d0
Tommy Long <pete_201@hotmail.com>
2016-02-17 22:42:36 +0000

    dropped hosting environment and introduced app env

--- src/Gilmond.Auth/Startup.cs
+++ src/Gilmond.Auth/Startup.cs
@@ -3,21 +3,23 @@ using Microsoft.AspNet.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.PlatformAbstractions;
 
 namespace Gilmond.Auth
 {
 	public class Startup
 	{
-		public Startup(IHostingEnvironment env)
+		private readonly IConfigurationRoot _configuration;
+		private readonly IApplicationEnvironment _environment;
+
+		public Startup(IApplicationEnvironment environment)
 		{
-			// Set up configuration sources.
 			var builder = new ConfigurationBuilder()
 				.AddJsonFile("appsettings.json")
 				.AddEnvironmentVariables();
-			Configuration = builder.Build();
+			_configuration = builder.Build();
+			_environment = environment;
 		}
-
-		public IConfigurationRoot Configuration { get; set; }
 
 		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services)
@@ -29,7 +31,7 @@ namespace Gilmond.Auth
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
 		public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
 		{
-			loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+			loggerFactory.AddConsole(_configuration.GetSection("Logging"));
 			loggerFactory.AddDebug();
 
 			app.UseIISPlatformHandler();
